3ace39e9150967c01ba772a7883aa4a50c7e54e9
Lucero Vargas <u202215301@upc.edu.pe>
2025-04-20 22:30:59 -0500

    Estoy editando la tabla Pago

--- SWParkingZone/src/main/java/pe/edu/upc/swparkingzone/repositories/IPagoRepository.java
+++ SWParkingZone/src/main/java/pe/edu/upc/swparkingzone/repositories/IPagoRepository.java
@@ -2,11 +2,8 @@ package pe.edu.upc.swparkingzone.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import pe.edu.upc.swparkingzone.entities.Pagos;
-
+import pe.edu.upc.swparkingzone.entities.Pago;
 
 @Repository
-
-public interface IPagosRepository extends JpaRepository<Pagos, Integer> {
-
+public interface IPagoRepository extends JpaRepository<Pago, Integer> {
 }
