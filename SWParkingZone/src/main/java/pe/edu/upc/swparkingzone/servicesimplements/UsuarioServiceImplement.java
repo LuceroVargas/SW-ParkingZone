fd30a57bec9ba52b89081be8a9cdbc3ee927246d
John William Machaca Saavedra <136102139+U202214514@users.noreply.github.com>
2025-04-20 21:42:13 -0500

    Creacion de entidades Usuario y Suscripcion.

---
+++ SWParkingZone/src/main/java/pe/edu/upc/swparkingzone/servicesimplements/UsuarioServiceImplement.java
@@ -0,0 +1,46 @@
+package pe.edu.upc.swparkingzone.servicesimplements;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pe.edu.upc.swparkingzone.entities.Usuario;
+import pe.edu.upc.swparkingzone.repositories.IUsuarioRepository;
+import pe.edu.upc.swparkingzone.servicesinterfaces.IUsuarioService;
+
+
+import java.util.List;
+
+@Service
+public class UsuarioServiceImplement implements IUsuarioService {
+
+    @Autowired
+    private IUsuarioRepository uR;
+    @Override
+    public List<Usuario> list() {
+        return uR.findAll();
+    }
+
+    @Override
+    public void insert(Usuario u) {
+        uR.save(u);
+    }
+
+    @Override
+    public Usuario listId(int id) {
+        return uR.findById(id).orElse(new Usuario());
+    }
+
+    @Override
+    public void update(Usuario u) {
+        uR.save(u);
+    }
+
+    @Override
+    public void delete(int id) {
+        uR.deleteById(id);
+    }
+
+    @Override
+    public List<Usuario> buscar(String nombre) {
+        return uR.buscarNombre(nombre);
+    }
+}
